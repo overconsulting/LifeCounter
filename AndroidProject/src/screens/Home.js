9670948f413fe5a132956eb2905d81cb828f9c2d
LouisYang95 <91289384+LouisYang95@users.noreply.github.com>
2022-10-04 01:44:05 +0200

    Adding input and some style to home

--- AndroidProject/src/screens/Home.js
+++ AndroidProject/src/screens/Home.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import {View, Text, TouchableOpacity, TextInput} from 'react-native';
 
 import stylesHome from '../styles/home';
 const Home = ({navigation}) => {
+    let [name] = React.useState('Louis');
   return (
     <View style={stylesHome.home}>
       <View style={stylesHome.home}>
         <Text>Home</Text>
-
-        <TouchableOpacity onPress={() => navigation.navigate('Game')} style={stylesHome.gameBtn1}>
+        <TouchableOpacity onPress={() => navigation.navigate('Game')}>
           <Text>Game</Text>
         </TouchableOpacity>
+        <TextInput style={stylesHome.textInput} value={name} onChange />
       </View>
     </View>
   );
--- AndroidProject/src/styles/home.js
+++ AndroidProject/src/styles/home.js
@@ -4,31 +4,12 @@ const styles = StyleSheet.create({
     home:{
         flex: 1,
     },
-    firstPart:{
-        flex: 1,
-        backgroundColor: 'red',
+    textInput:{
+        borderWidth: 1,
+        borderRadius: 5,
+        width: 200,
+        height: 40,
+        fontSize: 20,
     },
-    secondPart:{
-        flex: 1,
-        backgroundColor: 'blue',
-    },
-    gameBtn1:{
-        flex: 1,
-        backgroundColor: 'blue',
-    },
-    gameBtn2:{
-        flex: 1,
-    },
-    player1:{
-        flex: 2,
-        color: 'white',
-        textAlign: 'center',
-        backgroundColor: 'black',
-        fontSize: 30,
-    },
-    text:{
-        color: 'white',
-        textAlign: 'center',
-    }
 });
 export default styles;
