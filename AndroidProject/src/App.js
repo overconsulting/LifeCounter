4275cf7cb6debcb9f298b352ca2f07675611f57c
LouisYang95 <91289384+LouisYang95@users.noreply.github.com>
2022-10-03 21:44:40 +0200

    Moving app.js and adding navigation of react native

---
+++ AndroidProject/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import HomeScreen from './screens/Home';
+import GameScreen from './screens/Game';
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Home Page'}} />
+        <Stack.Screen name="Game" component={GameScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
