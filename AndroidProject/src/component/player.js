81ca5d4562887f5712c1ad47dc405d065c5b2bc7
LouisYang95 <91289384+LouisYang95@users.noreply.github.com>
2022-10-05 15:24:33 +0200

    Adding function to addlifepoint and to remove lifepoint

--- AndroidProject/src/component/player.js
+++ AndroidProject/src/component/player.js
@@ -1,45 +1,60 @@
-import React, { useState, useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text, TouchableOpacity, Image} from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 import stylesGame from '../styles/game';
-const Player = ({playerId,color}) => {
-    const [player,setPlayer] = useState('')
-    useEffect(() =>{
-        const getPlayer = async () =>{
-            const players = await AsyncStorage.getItem('Player'+playerId)
-            setPlayer(JSON.parse(players))
-            console.log(JSON.stringify(players))
-        }
-        getPlayer()
-    },[]);
+const Player = ({playerId}) => {
+  const [player, setPlayer] = useState('');
+
+  useEffect(() => {
+    const getPlayer = async () => {
+      const players = await AsyncStorage.getItem('Player' + playerId);
+      setPlayer(JSON.parse(players));
+    };
+    getPlayer();
+  }, []);
+
+  const addLifePoint = () => {
+    let lifePoint = player.lifePoint;
+    lifePoint++;
+    setPlayer({...player, lifePoint: lifePoint});
+    AsyncStorage.setItem('Player' + playerId, JSON.stringify(player));
+  };
+
+  const minusLifePoint = () => {
+    let lifePoint = player.lifePoint;
+    lifePoint--;
+    setPlayer({...player, lifePoint: lifePoint});
+    AsyncStorage.setItem('Player' + playerId, JSON.stringify(player));
+  };
+
+
   return (
-    <View style={color}>
-      <View style={color}>
+    <View style={player.color}>
+      <View style={player.color}>
         <TouchableOpacity
-          style={stylesGame.gameBtn1}
-        //   onPress={() => plusLifePoint(lifePoint1)}
-          >
-          <Image
+          style={stylesGame.gameBtn}
+          onPress={() => addLifePoint()}>
+          {/* <Image
             source={require('../img/814208.png')}
             style={stylesGame.image}
-          />
-          <Text style={stylesGame.text}> Game screen </Text>
+          /> */}
+          <Icon name="rocket" color="white" size={30} ></Icon>
         </TouchableOpacity>
       </View>
-      <View style={color}>
+      <View style={player.color}>
         <Text style={stylesGame.player1}>{player.lifePoint}</Text>
         <Text style={stylesGame.player1}>{player.name}</Text>
       </View>
-      <View style={color}>
+      <View style={player.color}>
         <TouchableOpacity
-          style={stylesGame.gameBtn1}
-        //   onPress={() => minusLifePoint(lifePoint1)}
-          >
+          style={stylesGame.gameBtn}
+          onPress={() => minusLifePoint()}>
           <Image
             source={require('../img/149163.png')}
             style={stylesGame.image}
           />
-          <Text style={stylesGame.text}> Game screen </Text>
         </TouchableOpacity>
       </View>
     </View>
