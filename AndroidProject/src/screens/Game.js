bb4957d6dd5be89b15dac07c0b4d91389ddd2732
LouisYang95 <91289384+LouisYang95@users.noreply.github.com>
2022-10-05 15:24:53 +0200

    Adapting screen according to users

--- AndroidProject/src/screens/Game.js
+++ AndroidProject/src/screens/Game.js
@@ -1,17 +1,91 @@
-import React from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, TouchableOpacity, Text} from 'react-native';
 
 //shared preferences / crypted
 import stylesGame from '../styles/game';
 import Player from '../component/player';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+const Game = ({navigation}) => {
+  const [nbPlayer, setNbPlayer] = useState('');
 
+  useEffect(() => {
+    const getNbPlayer = async () => {
+      const nbPlayer = await AsyncStorage.getItem('nbPlayer');
+      setNbPlayer(nbPlayer);
+    };
+    getNbPlayer();
+  }, []);
 
-const Game = () => {
+  const playBoard = () => {
+    if (nbPlayer == '2') {
+      return (
+        <View style={stylesGame.main}>
+          <View style={stylesGame.container2players}>
+            <Player playerId="1" />
+          </View>
+          <View style={stylesGame.btnContainer}>
+            <TouchableOpacity
+              style={stylesGame.gameBtn}
+              onPress={() => {
+                navigation.navigate('Pregame');
+              }}>
+              <Text style={stylesGame.text}>Back to lobby</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={stylesGame.container2players}>
+            <Player playerId="2" />
+          </View>
+        </View>
+      );
+    } else if (nbPlayer == '3') {
+      return (
+        <View style={stylesGame.container4players}>
+          <View style={stylesGame.container3players}>
+            <Player playerId="1" />
+            <Player playerId="2" />
+          </View>
+          <View style={stylesGame.btnContainer}>
+            <TouchableOpacity
+              style={stylesGame.gameBtn}
+              onPress={() => {
+                navigation.navigate('Pregame');
+              }}>
+              <Text style={stylesGame.text}>Back to lobby</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={stylesGame.container3players3}>
+            <Player playerId="3" />
+          </View>
+        </View>
+      );
+    } else if (nbPlayer == '4') {
+      return (
+        <View style={stylesGame.container4players}>
+          <View style={stylesGame.container3players}>
+            <Player playerId="1" />
+            <Player playerId="2" />
+          </View>
+          <View style={stylesGame.btnContainer}>
+            <TouchableOpacity
+              style={stylesGame.gameBtn}
+              onPress={() => {
+                navigation.navigate('Pregame');
+              }}>
+              <Text style={stylesGame.text}>Back to lobby</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={stylesGame.container3players}>
+            <Player playerId="3" />
+            <Player playerId="4" />
+          </View>
+        </View>
+      );
+    }
+  };
+
   return (
-    <View style={stylesGame.container}>
-      <Player playerId="1" color={stylesGame.firstPart}/>
-      <Player playerId="2" color={stylesGame.secondPart}/>
+    <View style={stylesGame.main}>
+      <View style={stylesGame.main}>{playBoard()}</View>
     </View>
   );
 };
