9d020b960eb9d778d66511bcf212c3f001e52d45
ldandoy <ldadoy@overconsulting>
2022-10-07 10:30:01 +0200

    Ajout de redux

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { Provider } from 'react-redux'
 
+import { store } from './store'
 import HomeScreen from './screens/Home';
 import GameScreen from './screens/Game';
 import PregameScreen from './screens/Pregame';
+
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}} />
-        <Stack.Screen name="Game" component={GameScreen} options={{headerShown: false}} />
-        <Stack.Screen name="Pregame" component={PregameScreen} options={{ title: 'Pregame Page' }} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}} />
+          <Stack.Screen name="Game" component={GameScreen} options={{headerShown: false}} />
+          <Stack.Screen name="Pregame" component={PregameScreen} options={{ title: 'Pregame Page' }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 };
 
---
+++ src/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+import {gameReducer} from './slices/game'
+
+export const store = configureStore({
+  reducer: {
+    game: gameReducer,
+  },
+})
